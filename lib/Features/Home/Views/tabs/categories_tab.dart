4ab449e77110c8018212961d0e90fc0bb08ef297
ahmed Desoky <84643423+ADesoky@users.noreply.github.com>
2023-11-05 00:45:33 +0200

    Task 2

--- lib/Features/Home/Views/tabs/categories_tab.dart
+++ lib/Features/Home/Views/tabs/categories_tab.dart
@@ -1,40 +1,81 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesTab extends StatelessWidget {
-   CategoriesTab({Key? key}) : super(key: key);
+import '../../Controlers/remote_service.dart';
+import '../../Models/model.dart';
 
+
+class CategoriesTab extends StatefulWidget {
+  CategoriesTab({Key? key}) : super(key: key);
+
+  @override
+  State<CategoriesTab> createState() => _CategoriesTabState();
+}
+
+class _CategoriesTabState extends State<CategoriesTab> {
+  int isLoaded = 0;
+  List<UsersModel> res =[];
+  Future<void> getUsersResponse() async {
+    const apiLink = 'https://jsonplaceholder.typicode.com/users';
+
+      final  response = await Dio().get(apiLink);
+      print('status : ${response.statusCode}');
+
+       res .addAll(
+           response.data.map((json) => UsersModel.fromJson(json)).toList());
+
+
+  }
+@override
+  void initState() {
+
+    // TODO: implement initState
+  getUsersResponse().then((value){
+
+    setState(() {
+isLoaded=1;
+    });
+  });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ListView.separated(
+      child: isLoaded == 1?ListView.separated(
         shrinkWrap: true,
-        itemCount: text.length,
+        itemCount: res.length,
         itemBuilder: (context, index) => Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Row(
             children: [
-              Image.asset(images[index]),
-              SizedBox(width: 8,),
-              Expanded(child: Text(text[index])),
+             CircleAvatar(child: Text(res[index].id.toString()),),
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(child: Text(res[index].name!)),
               Spacer(),
               IconButton(onPressed: () {}, icon: Icon(Icons.arrow_forward)),
             ],
           ),
         ),
-        separatorBuilder: (context , index)=>Divider(),
-      ),
+        separatorBuilder: (context, index) => Divider(),
+      ):Center(child:CircularProgressIndicator()) ,
     );
   }
-   List<String> text = [
+
+  List<String> text = [
     "Constructions",
     "Insurances",
     "Legal",
     "Buy & Sell",
     "Accounting Services",
   ];
-   List<String> images = [
+
+  List<String> images = [
     "assets/images/construction.png",
     "assets/images/Cate.png",
     "assets/images/legal.png",
