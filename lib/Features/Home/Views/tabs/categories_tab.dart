a1fdd31bc9718cc1be5446a3f721481e121eefe1
ahmed Desoky <84643423+ADesoky@users.noreply.github.com>
2023-11-05 01:39:03 +0200

    Task 22

--- lib/Features/Home/Views/tabs/categories_tab.dart
+++ lib/Features/Home/Views/tabs/categories_tab.dart
@@ -1,12 +1,7 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../Controlers/remote_service.dart';
 import '../../Models/model.dart';
-
 
 class CategoriesTab extends StatefulWidget {
   CategoriesTab({Key? key}) : super(key: key);
@@ -17,69 +12,67 @@ class CategoriesTab extends StatefulWidget {
 
 class _CategoriesTabState extends State<CategoriesTab> {
   int isLoaded = 0;
-  List<UsersModel> res =[];
-  Future<void> getUsersResponse() async {
+  List<UsersModel> res = [];
+  Future<int?> getUsersResponse() async {
     const apiLink = 'https://jsonplaceholder.typicode.com/users';
+    try {
+      final response = await Dio().get(apiLink);
 
-      final  response = await Dio().get(apiLink);
-      print('status : ${response.statusCode}');
+      List listaya = response.data;
+      for (var element in listaya) {
+        res.add(UsersModel.fromJson(element));
+      }
 
-       res .addAll(
-           response.data.map((json) => UsersModel.fromJson(json)).toList());
-
-
+      setState(() {
+        isLoaded = 1;
+      });
+    } catch (e) {
+      setState(() {
+        isLoaded = 2;
+      });
+      throw Exception(e);
+    }
   }
-@override
+
+  @override
   void initState() {
-
     // TODO: implement initState
-  getUsersResponse().then((value){
-
-    setState(() {
-isLoaded=1;
-    });
-  });
+    getUsersResponse();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: isLoaded == 1?ListView.separated(
+      child: isLoaded == 1
+          ? ListView.separated(
         shrinkWrap: true,
         itemCount: res.length,
         itemBuilder: (context, index) => Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Row(
             children: [
-             CircleAvatar(child: Text(res[index].id.toString()),),
+              CircleAvatar(
+                child: Text(res[index].id.toString()),
+              ),
               SizedBox(
                 width: 8,
               ),
               Expanded(child: Text(res[index].name!)),
               Spacer(),
-              IconButton(onPressed: () {}, icon: Icon(Icons.arrow_forward)),
+              IconButton(
+                  onPressed: () {}, icon: Icon(Icons.arrow_forward)),
             ],
           ),
         ),
         separatorBuilder: (context, index) => Divider(),
-      ):Center(child:CircularProgressIndicator()) ,
+      )
+          : isLoaded == 0
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+        child: Text('Failed to load API'),
+      ),
     );
   }
-
-  List<String> text = [
-    "Constructions",
-    "Insurances",
-    "Legal",
-    "Buy & Sell",
-    "Accounting Services",
-  ];
-
-  List<String> images = [
-    "assets/images/construction.png",
-    "assets/images/Cate.png",
-    "assets/images/legal.png",
-    "assets/images/buy.png",
-    "assets/images/account.png",
-  ];
 }
